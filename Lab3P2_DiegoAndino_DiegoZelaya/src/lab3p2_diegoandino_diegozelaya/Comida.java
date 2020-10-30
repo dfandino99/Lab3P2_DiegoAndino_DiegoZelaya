094f9fe6c02f535c35a8cf62429a790d882b890b
DiegoZelaya <69058652+DiegoZelaya@users.noreply.github.com>
2020-10-30 15:38:15 -0600

    Commit #3 DiegoZelaya

--- Lab3P2_DiegoAndino_DiegoZelaya/src/lab3p2_diegoandino_diegozelaya/Comida.java
+++ Lab3P2_DiegoAndino_DiegoZelaya/src/lab3p2_diegoandino_diegozelaya/Comida.java
@@ -2,6 +2,47 @@
 
 package lab3p2_diegoandino_diegozelaya;
 
+import java.util.Date;
+
 public class Comida extends Productos {
+    private String tipo;
+    private Date vencimiento;
+
+    public Comida() {
+        super();
+    }
+
+    public Comida(String tipo, Date vencimiento) {
+        this.tipo = tipo;
+        this.vencimiento = vencimiento;
+    }
+
+    public Comida(String tipo, Date vencimiento, String nombre, String descripcion, double precio) {
+        super(nombre, descripcion, precio);
+        this.tipo = tipo;
+        this.vencimiento = vencimiento;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Date getVencimiento() {
+        return vencimiento;
+    }
+
+    public void setVencimiento(Date vencimiento) {
+        this.vencimiento = vencimiento;
+    }
+
+    @Override
+    public String toString() {
+        return "Comida{" + "tipo=" + tipo + ", vencimiento=" + vencimiento + '}';
+    }
+    
     
 }
